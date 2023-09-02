acff95e8bf9b6f239fab33e73e1c08d0b67d0e39
Priya Malik <95068160+PriyaDeveloper630@users.noreply.github.com>
2023-09-02 22:20:35 +0530

    Update teams.js

--- teams.js
+++ teams.js
@@ -1,21 +1,14 @@
-var TrandingSlider = new Swiper('.tranding-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: false,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2.5,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+  const image = card.querySelector('.image');
+  const holo = card.querySelector('.holo');
+
+  card.addEventListener('mouseenter', () => {
+    holo.style.opacity = 1;
   });
+
+  card.addEventListener('mouseleave', () => {
+    holo.style.opacity = 0;
+  });
+});
